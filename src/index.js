64735e032d38d40ffbb8be899447bc44e0671a5f
Ted Conn <tconn@salesforce.com>
2020-09-18 16:25:49 +0200

    wip: get element by id

--- src/index.js
+++ src/index.js
@@ -302,10 +302,16 @@ function getElementsByClassName (classNames, context = document) {
   return getMatchingElementsByClassName(elementIterator, classNamesSplit, context)
 }
 
+function getElementById (id) {
+  const elementIterator = new ElementIterator(document)
+  return getMatchingElementById(elementIterator)
+}
+
 export {
   querySelectorAll,
   querySelector,
   getElementsByTagName,
   getElementsByTagNameNS,
-  getElementsByClassName
+  getElementsByClassName,
+  getElementById
 }
